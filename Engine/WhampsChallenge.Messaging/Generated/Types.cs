08d71b4c4efdf87bb10d1f7544578f75e7bd50a5
Palcer, Markus <markus.palcer@gmail.com>
2019-03-23 12:46:30 +0100

    Auto-Running .tt-Files on pre-build

---
+++ Engine/WhampsChallenge.Messaging/Generated/Types.cs
@@ -0,0 +1,42 @@
+﻿// This file contains all enums as declared by the contracts.json file
+
+// Generated file, so ReSharper-Checks are disabled.
+// ReSharper disable all
+
+using System;
+
+
+namespace WhampsChallenge.Messaging.Level3 {
+	public class GameState {
+		Boolean HasArrow { get; set; }
+		Int32 MovesLeft { get; set; }
+	}
+
+	public class Result {
+		Perception[] Perceptions { get; set; }
+		GameState GameState { get; set; }
+	}
+
+}
+namespace WhampsChallenge.Messaging.Level2 {
+	public class GameState {
+		Int32 MovesLeft { get; set; }
+	}
+
+	public class Result {
+		Perception[] Perceptions { get; set; }
+		GameState GameState { get; set; }
+	}
+
+}
+namespace WhampsChallenge.Messaging.Level1 {
+	public class GameState {
+		Int32 MovesLeft { get; set; }
+	}
+
+	public class Result {
+		Perception[] Perceptions { get; set; }
+		GameState GameState { get; set; }
+	}
+
+}
---
+++ Library/CSharp/WhampsChallenge.Library/Generated/Actions.cs
@@ -0,0 +1,78 @@
+﻿// Generated file, so ReSharper-Checks are disabled. 
+// ReSharper disable all
+
+
+namespace WhampsChallenge.Library.Level3.Actions {
+
+	using WhampsChallenge.Library.Level3.Enums;
+	using WhampsChallenge.Library.Level3.Types;
+
+	internal class Move {
+
+		public string Action => "Move";
+
+		public WhampsChallenge.Shared.Maps.FourDirections.Direction Direction { get; set; }
+
+	}
+
+	internal class Pickup {
+
+		public string Action => "Pickup";
+
+
+	}
+
+	internal class Shoot {
+
+		public string Action => "Shoot";
+
+		public WhampsChallenge.Shared.Maps.FourDirections.Direction Direction { get; set; }
+
+	}
+
+}
+
+namespace WhampsChallenge.Library.Level2.Actions {
+
+	using WhampsChallenge.Library.Level2.Enums;
+	using WhampsChallenge.Library.Level2.Types;
+
+	internal class Move {
+
+		public string Action => "Move";
+
+		public WhampsChallenge.Shared.Maps.FourDirections.Direction Direction { get; set; }
+
+	}
+
+	internal class Pickup {
+
+		public string Action => "Pickup";
+
+
+	}
+
+}
+
+namespace WhampsChallenge.Library.Level1.Actions {
+
+	using WhampsChallenge.Library.Level1.Enums;
+	using WhampsChallenge.Library.Level1.Types;
+
+	internal class Move {
+
+		public string Action => "Move";
+
+		public WhampsChallenge.Shared.Maps.FourDirections.Direction Direction { get; set; }
+
+	}
+
+	internal class Pickup {
+
+		public string Action => "Pickup";
+
+
+	}
+
+}
+

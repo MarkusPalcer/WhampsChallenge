bde9b9e6f720bc0e324a2a6846b34bc25519857b
Palcer, Markus <markus.palcer@gmail.com>
2020-05-05 16:17:34 +0200

    Using generic action decoder base class

---
+++ Engine/WhampsChallenge.Messaging/Common/ActionDecoder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using WhampsChallenge.Core.Common;
+
+namespace WhampsChallenge.Messaging.Common
+{
+    public abstract class ActionDecoder : IActionDecoder
+    {
+        protected Dictionary<string, Type> registeredTypes = new Dictionary<string, Type>();
+
+        public IAction Decode(JObject message)
+        {
+            var actionName = message["Action"].Value<string>();
+            var actionType = registeredTypes[actionName];
+            return (IAction) message.ToObject(actionType);
+        }
+    }
+}
